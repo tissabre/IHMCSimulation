92bb051059ae0bcad41155103f577a5924138073
Nas <tissabre@vt.edu>
2019-04-16 14:46:56 -0400

    Update motorController.ino

--- Arduino/motorController/motorController.ino
+++ Arduino/motorController/motorController.ino
@@ -27,45 +27,26 @@ float new_max_angle = 6.23;
 #define ID2 5
 #define ID3 3
 #define SERVO_DARWIN_BAUD 1000000
-#define BIGGEST_ID 18
+#define BIGGEST_ID 20
 const long TWOBYTES =  pow(2,16)-1;
 
 class Dynamixel {
   private:
     int ID;
-    //angles in radians
-    float MIN_ANGLE;
-    float MAX_ANGLE;
 
   public:
     //Constructors
-    Dynamixel(int _id, float min_rad, float max_rad) {
+    Dynamixel(int _id) {
       ID = _id;
-      MIN_ANGLE = min_rad;
-      MAX_ANGLE = max_rad;
     }
 
     int id() {
       return ID;
     }
 
-    float minAngle() {
-      return MIN_ANGLE;
-    }
-
-    float maxAngle() {
-      return MAX_ANGLE;
-    }
-
     void info() {
       Serial.print("ID: ");
       Serial.println(ID);
-
-      Serial.print("Minimum Angle: ");
-      Serial.println(MIN_ANGLE);
-
-      Serial.print("Maximum Angle: ");
-      Serial.println(MAX_ANGLE);
     }
 };
 
@@ -74,14 +55,18 @@ class Dynamixel {
 //Syntax: Dynamixel JOINT_NAME(ID, MIN_ANGLE, MAX_ANGLE);
 
 //ARMS
-Dynamixel SHOULDER_R(100, 3.17, 6.27),            HIGH_ARM_R(3, 2.33, 5.13),              LOW_ARM_R(5, 1.6, 3.74),
-          SHOULDER_L(2, 2 * PI - 6.27, 2 * PI - 3.17),    HIGH_ARM_L(4, 2 * PI - 5.13, 2 * PI - 2.33),    LOW_ARM_L(6, 2 * PI - 3.74, 2 * PI - 1.6);
+Dynamixel SHOULDER_R(100),  HIGH_ARM_R(3),    LOW_ARM_R(5),
+          SHOULDER_L(2),    HIGH_ARM_L(4),    LOW_ARM_L(6);
 
 //LEGS
-Dynamixel THIGH1_R(7, 3.17, 6.27),                PELVIS_R(9, 2.33, 5.13),                THIGH2_R(11, 1.6, 3.74),                        TIBIA_R(13, 0, 0),                      ANKLE1_R(15, 0, 0),                     ANKLE2_R(17, 0, 0),
-          THIGH1_L(8, 2 * PI - 6.27, 2 * PI - 3.17),      PELVIS_L(10, 2 * PI - 5.13, 2 * PI - 2.33),     THIGH2_L(12, 2 * PI - 3.74, 2 * PI - 1.6),            TIBIA_L(14, 2 * PI - 0, 0),                 ANKLE1_L(16, 2 * PI - 0, 0),                ANKLE2_L(18, 2 * PI - 0, 0);
+Dynamixel THIGH1_R(7),      PELVIS_R(9),      THIGH2_R(11),    TIBIA_R(13),       ANKLE1_R(15),         ANKLE2_R(17),
+          THIGH1_L(8),      PELVIS_L(10),     THIGH2_L(12),    TIBIA_L(14),       ANKLE1_L(16),         ANKLE2_L(18);
 
-Dynamixel allMotors[] = {SHOULDER_R, HIGH_ARM_R, LOW_ARM_R};//, HIGH_ARM_R, HIGH_ARM_L, LOW_ARM_R, LOW_ARM_L, THIGH1_R, THIGH1_L, PELVIS_R, PELVIS_L, THIGH2_R, THIGH2_L, TIBIA_R, TIBIA_L, ANKLE1_R, ANKLE1_L, ANKLE2_R, ANKLE2_L};
+//NECK
+Dynamixel PAN(19),          TILT(20);
+
+//Array of motors to send
+Dynamixel allMotors[] = {TILT, SHOULDER_R, SHOULDER_L, HIGH_ARM_R, HIGH_ARM_L, LOW_ARM_R, LOW_ARM_L, PAN};//, HIGH_ARM_R, HIGH_ARM_L, LOW_ARM_R, LOW_ARM_L, THIGH1_R, THIGH1_L, PELVIS_R, PELVIS_L, THIGH2_R, THIGH2_L, TIBIA_R, TIBIA_L, ANKLE1_R, ANKLE1_L, ANKLE2_R, ANKLE2_L};
 
 void setup() {
   // put your setup code here, to run once:
@@ -94,8 +79,8 @@ void setup() {
 
   //  servo_set(HIGH_ARM_R.id(), SERVO_REGISTER_GOAL_ANGLE, HIGH_ARM_R.minAngle(), timeout);
   //  servo_get(3,SERVO_REGISTER_PRESENT_ANGLE,&motorCopy,timeout);
-  //    servo_loosen_motor(18);
-//    servo_changeID_from_to(100,3);
+//      servo_loosen_motor(20);
+//    servo_changeID_from_to(100,2);
 
   //Only need to run once
 //  loosenMotors(1, 6);
@@ -103,20 +88,20 @@ void setup() {
 }
 
 void loop() {
-//  delay(2);
-//  Micros = micros();
-//  if (Micros - PreviousCycle >= 50)
-//  {
-//    PreviousCycle = Micros;
-//    EASYCAT.MainTask();                                 // execute the EasyCAT task
-//    Application();
-//
-//}
+  delay(2);
+  Micros = micros();
+  if (Micros - PreviousCycle >= 50)
+  {
+    PreviousCycle = Micros;
+    EASYCAT.MainTask();                                 // execute the EasyCAT task
+    Application();
 
-  //  helloMotion();
+}
+
+//    helloMotion();
 //    mimicArm();                                                                                                          
   //  mimicLeg();
-    testDynamixelRead(1, BIGGEST_ID);
+//    testDynamixelRead(1, BIGGEST_ID);
 }
 
 void helloMotion() {
@@ -260,10 +245,17 @@ void SENDPOSITIONS() {
     whole2bytes = radianto2bytes(READ(dy));
     b8_15 = (whole2bytes & 0xFF00) >> 8;
     b0_7 = whole2bytes & 0xFF;
+    //whole2bytes = (b8_15 << 8) | b0_7;
+    //Exceptions
     if (dy.id() == 100)
       EASYCAT.BufferIn.Byte[3*eth_pos] = 1;
+    else if (dy.id() == 19)
+      EASYCAT.BufferIn.Byte[3*eth_pos] = 7;
+    else if (dy.id() == 20)
+      EASYCAT.BufferIn.Byte[3*eth_pos] = 0;
     else
       EASYCAT.BufferIn.Byte[3*eth_pos] = dy.id();
+      
     EASYCAT.BufferIn.Byte[3*eth_pos+1] = b8_15;
     EASYCAT.BufferIn.Byte[3*eth_pos+2] = b0_7;
     //////////////////////////////////////////////
@@ -296,6 +288,13 @@ long radianto2bytes (float rad) {
 
 void Application()
 {
+  
+  
+//  mimic
+//  MOVE(SHOULDER_L,  2 * PI - READ(SHOULDER_R));
+//  MOVE(HIGH_ARM_L,  2 * PI - READ(HIGH_ARM_R));
+//  MOVE(LOW_ARM_L,   2 * PI - READ(LOW_ARM_R));  
+
   SENDPOSITIONS();
 }
 
